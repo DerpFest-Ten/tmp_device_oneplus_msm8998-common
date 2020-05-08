DerpFest-Devices/device_oneplus_msm8998-common

msm8998-common: DerpFest bringup
 * Update extract-utils
 * Update dependencies
 * Modify overlays

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

common.mk
@@ -26,7 +26,9 @@ $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosip
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \

DerpFest-Ten/tmp_device_oneplus_msm8998-common

common.mk
@@ -30,7 +30,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-derp
 
 # Permissions
 PRODUCT_COPY_FILES += \
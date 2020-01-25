b9800e7d6757531ebb9295e15efdbc55aa9c68c7
NurKeinNeid <mralexman3000@gmail.com>
2020-01-25 18:26:54 +0100

    msm8998-common: DerpFest bringup
     * Update extract-utils
     * Update dependencies
     * Modify overlays
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -30,7 +30,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-derp
 
 # Permissions
 PRODUCT_COPY_FILES += \
